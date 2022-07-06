5320d4d59214cf982237293c19b22915a50747f1
Marvin Mednick <mmednick@gmail.com>
2022-07-05 17:47:24 -0700

    WIP

--- src/mwis.rs
+++ src/mwis.rs
@@ -6,6 +6,7 @@ use std::collections::HashMap;
 //#[derive(Debug)]
 pub struct MWISInfo {
     vertex_list: HashMap::<usize,u64>,
+    mwis_results: HashMap::<usize,u64>,
 }
 
 
@@ -15,15 +16,33 @@ impl MWISInfo {
     pub fn new() -> Self {
         MWISInfo {
             vertex_list : HashMap::<usize,u64>::new(),
+            mwis_results : HashMap::<usize,u64>::new(),
         }
         
     }
 
     pub fn add_vertex(&mut self,weight:u64) -> usize {
-        let id = self.vertex_list.len();
+        // vertexes are number 1-N
+        let id = self.vertex_list.len() + 1;
         self.vertex_list.insert(id,weight);
         debug!("Added Simple Vertex {}  w={}",id,weight);
         id
+    }
+
+    pub fn compute_mwis(&mut self) {
+
+        self.mwis_results.insert(0,0);
+        let w1 = self.vertex_list.get(&1).unwrap().clone();
+        if self.vertex_list.len() > 0 {
+            self.mwis_results.insert(1,w1);
+        }
+
+        let num_vertex = self.vertex_list.len();
+        for index in 2..num_vertex {
+            debug!("index {}", index);
+            
+        }
+
     }
 
 }
@@ -49,10 +68,10 @@ mod tests {
     fn setup_basic() -> MWISInfo {
 
         let mut h = MWISInfo::new();
-        assert_eq!(h.add_vertex(10),0);
-        assert_eq!(h.add_vertex(20),1);
-        assert_eq!(h.add_vertex(30),2);
-        assert_eq!(h.add_vertex(5),3);
+        assert_eq!(h.add_vertex(10),1);
+        assert_eq!(h.add_vertex(20),2);
+        assert_eq!(h.add_vertex(30),3);
+        assert_eq!(h.add_vertex(5),4);
         h
     }
 
@@ -60,6 +79,7 @@ mod tests {
     fn test_add() {
         init();
         let mut h = setup_basic();
+        h.compute_mwis();
 
     }
 
